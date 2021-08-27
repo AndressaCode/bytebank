dae5e1a615939c1db93d392d5660fdef82d55647
Andressa Souza <amw.andressa@gmail.com>
2021-08-27 00:20:57 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/br/andressa/bytebank/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.br.andressa.bytebank
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatelessWidget {
+  const ContactsList({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+    );
+  }
+}
