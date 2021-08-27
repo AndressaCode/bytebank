34ee8425c7fc9fb7e889e0244938ce6014e335f7
Andressa Souza <amw.andressa@gmail.com>
2021-08-27 00:33:59 -0300

    integrando as pages dashboard e contactslist

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -9,6 +9,32 @@ class ContactsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Contacts'),
       ),
+      body: ListView(
+        children: [
+          Card(
+            child: ListTile(
+              title: Text(
+                'Alex',
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+              ),
+              subtitle: Text(
+                '1000',
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(
+          Icons.add
+        ),
+      ),
     );
   }
 }
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,5 +1,4 @@
-
-
+import 'package:bytebank/screens/contacts_list.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
@@ -21,25 +20,39 @@ class Dashboard extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              height: 100.0,
-              width: 150.0,
+            child: Material(
               color: Theme.of(context).primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(Icons.people,
-                    color: Colors.white,
-                    size: 24.0,
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ContactsList(),
+                    ),
+                  );
+                },
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  height: 100.0,
+                  width: 150.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.people,
+                        color: Colors.white,
+                        size: 24.0,
+                      ),
+                      Text(
+                        'Contacts',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ],
                   ),
-                  Text('Contacts',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                    ),),
-                ],
+                ),
               ),
             ),
           ),
