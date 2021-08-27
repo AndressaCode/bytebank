25d46a6d17a33ec53cbe4d8802a18e41aecc7b25
Andressa Souza <amw.andressa@gmail.com>
2021-08-27 01:06:09 -0300

    Integrando o model do contact com o botão de inserção

--- lib/screens/contact_form.dart
+++ lib/screens/contact_form.dart
@@ -1,7 +1,17 @@
+import 'package:bytebank/models/contact.dart';
 import 'package:flutter/material.dart';
 
-class ContactForm extends StatelessWidget {
+class ContactForm extends StatefulWidget {
   const ContactForm({Key key}) : super(key: key);
+
+  @override
+  _ContactFormState createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _accountNumberController =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +26,7 @@ class ContactForm extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(top: 8.0),
               child: TextField(
+                controller: _nameController,
                 decoration: InputDecoration(
                   labelText: 'Full Name',
                 ),
@@ -27,6 +38,7 @@ class ContactForm extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(top: 8.0),
               child: TextField(
+                controller: _accountNumberController,
                 decoration: InputDecoration(
                   labelText: 'Account number',
                 ),
@@ -41,7 +53,13 @@ class ContactForm extends StatelessWidget {
               child: SizedBox(
                 width: double.maxFinite,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    final String name = _nameController.text;
+                    final int accountNumber =
+                        int.tryParse(_accountNumberController.text);
+                    final Contact newContact = new Contact(name, accountNumber);
+                    Navigator.pop(context, newContact);
+                  },
                   child: Text('Create'),
                 ),
               ),
--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
@@ -30,10 +31,18 @@ class ContactsList extends StatelessWidget {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Icon(
-          Icons.add
-        ),
+        onPressed: () {
+          Navigator.of(context)
+              .push(
+                MaterialPageRoute(
+                  builder: (context) => ContactForm(),
+                ),
+              )
+              .then(
+                (newContact) => debugPrint(newContact.toString()),
+              );
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
