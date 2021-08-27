d0c91abaec0c352f146866ac25b6d0d530849935
Andressa Souza <amw.andressa@gmail.com>
2021-08-27 00:49:28 -0300

    Adição do style do bottão

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/dashboard.dart';
@@ -19,7 +20,7 @@ class BytebankApp extends StatelessWidget {
           textTheme: ButtonTextTheme.primary,
         ),
       ),
-      home: Dashboard(),
+      home: ContactForm(),
     );
   }
 }
