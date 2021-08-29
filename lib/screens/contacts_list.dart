253b6be19e50e1da86fbdf83fb30dbf28a67ffd6
Andressa Souza <amw.andressa@gmail.com>
2021-08-29 02:08:03 -0300

    Implementação de boas práticas na usabilidade para o usuário (Mostrando estado do andamento de sua solicitação)

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -4,38 +4,54 @@ import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Contacts'),
       ),
       body: FutureBuilder<List<Contact>>(
         initialData: [],
-        future: Future.delayed(Duration(seconds: 1)).then((value) => findAll()),
-        builder: (context, snapshot){
-            final List<Contact> contacts = snapshot.data;
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                final Contact contact = contacts[index];
-                return _ContactItem(contact);
-              },
-              itemCount: contacts.length,
-            );
+        future: findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircularProgressIndicator(),
+                    SizedBox(height: 16.0),
+                    Text('Loading...'),
+                  ],
+                ),
+              );
+              break;
+            case ConnectionState.active:
+              // TODO: Handle this case.
+              // Mostra trecho do que está acontecendo..
+              // Tipo o download, mostra 10%, 25% e assim sucessivamente...
+              break;
+            case ConnectionState.done:
+              final List<Contact> contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts[index];
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts.length,
+              );
+              break;
           }
-          // return Center(
-          //   child: Column(
-          //     mainAxisAlignment: MainAxisAlignment.center,
-          //     crossAxisAlignment: CrossAxisAlignment.center,
-          //     children: [
-          //       CircularProgressIndicator(),
-          //       Text('Loading...'),
-          //     ],
-          //   ),
-          // );
+          // Tende não acontecer mas é boa prática colocar
+          // Pois todos os cases estão preenchidos,
+          // a probabilidade de ocorrer isso é mínima
+          return Text('Unknown error');
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -55,8 +71,7 @@ class ContactsList extends StatelessWidget {
   }
 }
 
-class _ContactItem extends StatelessWidget{
-
+class _ContactItem extends StatelessWidget {
   final Contact contact;
 
   _ContactItem(this.contact);
@@ -80,5 +95,4 @@ class _ContactItem extends StatelessWidget{
       ),
     );
   }
-
 }
