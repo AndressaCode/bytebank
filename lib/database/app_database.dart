cc30c9b1b938b142c2dd43f97e9eaaa6f1f8c183
Andressa Souza <amw.andressa@gmail.com>
2021-08-29 00:06:57 -0300

    Integrando o banco de dados com a tela do contact_list

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,10 +1,36 @@
+import 'package:bytebank/models/contact.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-void createDatabase() {
-  // caminho do bd
-  getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'bytebank.db');
-    openDatabase(path,)
-  });
+Future<Database> getDatabase() async {
+  final String path = join(await getDatabasesPath(), 'byteBank01.db');
+  return openDatabase(path, onCreate: (db, version) {
+    db.execute('CREATE TABLE contacts('
+        'id INTEGER PRIMARY KEY, '
+        'name TEXT, '
+        'account_number INTEGER)');
+  }, version: 1);
+}
+
+// Inserir contato no db
+Future<int> save(Contact contact) async {
+  final Database db = await getDatabase();
+  final Map<String, dynamic> contactMap = Map();
+  contactMap['name'] = contact.name;
+  contactMap['account_number'] = contact.accountNumber;
+  return db.insert('contacts', contactMap);
+}
+
+// Buscar contato no db
+Future<List<Contact>> findAll() async {
+  final Database db = await getDatabase();
+  final List<Map<String, dynamic>> result = await db.query('contacts');
+  final List<Contact> contacts = [];
+  for (Map<String, dynamic> row in result) {
+  final Contact contact =
+  Contact(row['id'], row['name'], row['account_number'],
+  );
+  contacts.add(contact);
+  }
+  return contacts;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
-import 'package:bytebank/screens/contact_form.dart';
+import 'package:bytebank/database/app_database.dart';
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/screens/contacts_list.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/dashboard.dart';
 
-void main() => runApp(BytebankApp());
+void main() {
+  runApp(BytebankApp());
+  save(Contact(0, 'alex', 1000)).then((id){
+    findAll().then((contacts) => debugPrint(contacts.toString()));
+  });
+}
 
 class BytebankApp extends StatelessWidget {
   const BytebankApp({Key key}) : super(key: key);
@@ -20,7 +27,7 @@ class BytebankApp extends StatelessWidget {
           textTheme: ButtonTextTheme.primary,
         ),
       ),
-      home: Dashboard(),
+      home: ContactsList(),
     );
   }
 }
